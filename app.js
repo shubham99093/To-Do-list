e3ea1720cf3a346ebe6fb9795c9645e06296dfbb
SHUBHAM CHODVADIYA <shubhamchodavadiya1122@gmail.com>
2024-01-14 00:41:54 +0530

    update #1

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
 const inputBox = document.querySelector("#input-box");
 const listContainer = document.querySelector("#list-container");
 const listHeader = document.querySelector(".Task-header");
-let all = document.querySelector("#all");
-let active = document.querySelector("#active");
-let complated = document.querySelector("#complated");
-let removeComplated = document.querySelector("#removeComplated");
 let TaskList = [];
 let TaskCount = 0;
-saveData();
 
-function addTask() {
+function taskCheck() {
     if (inputBox.value === "") {
         alert("You Must Write Something!");
     }
@@ -17,39 +12,36 @@ function addTask() {
         alert("You added Same Task!");
     }
     else {
-        TaskList[TaskCount] = inputBox.value;
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        li.classList.add("Task");
-        li.classList.add("unchecked");
-        listContainer.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7"
-        inputBox.value = "";
-        li.appendChild(span);
-        TaskCount++;
-        if (TaskCount > 0) {
-            listHeader.style.display = "inline";
-        }
+        addTask(inputBox.value);
     }
-    saveData()
+    showCount();
+    saveData();
 }
 
-inputBox.addEventListener("keyup", function (event) {
-    if (event.code === "Enter") {
-        addTask();
+let addTask = (inputText) => {
+    TaskList.push(inputText);
+    let li = document.createElement("li");
+    li.innerHTML = inputText;
+    li.classList.add("Task");
+    li.classList.add("unchecked");
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7"
+    inputBox.value = "";
+    li.appendChild(span);
+    if (TaskList.length > 0) {
+        listHeader.style.display = "inline";
     }
+}
+
+inputBox.addEventListener("keyup", (event) => {
+    (event.code === "Enter") ? taskCheck() : null;
 });
 
-//arr method
 function chkSameTask(inputTask) {
-    if (TaskCount === 0) {
-        return false;
-    } else {
-        for (let i = 0; i < TaskCount; i++) {
-            if (TaskList[i] === inputTask) {
-                return true;
-            }
+    if (!(TaskList.length === 0)) {
+        if (TaskList.includes(inputTask) === true) {
+            return true;
         }
         return false;
     }
@@ -65,44 +57,40 @@ listContainer.addEventListener("click", function (e) {
         let index = TaskList.indexOf(Task);
         TaskList.splice(index, 1);
         e.target.parentElement.remove();
-        TaskCount--;
-        if (TaskCount == 0) {
+        if (TaskList.length == 0) {
             listHeader.style.display = "none";
         }
     }
-    saveData()
+    showCount();
+    saveData();
 }, false);
 
-all.addEventListener("click", function () {
+let allTask = () => {
     let Tasks = document.querySelectorAll(".Task");
     for (let task of Tasks) {
-        task.style.display = "list-item";
+        task.style.display = "block";
     }
-});
+}
 
-active.addEventListener("click", function () {
+let active = () => {
     let Tasks = document.querySelectorAll(".Task");
     for (let task of Tasks) {
-        if (task.classList[1] === "checked") {
-            task.style.display = "none";
-        } else if (task.classList[1] === "unchecked") {
-            task.style.display = "list-item";
-        }
+        (task.classList[1] === "checked") ?
+            task.style.display = "none"
+            : task.style.display = "block";
     }
-});
+}
 
-complated.addEventListener("click", function () {
+let complated = () => {
     let Tasks = document.querySelectorAll(".Task");
     for (let task of Tasks) {
-        if (task.classList[1] === "checked") {
-            task.style.display = "list-item";
-        } else if (task.classList[1] === "unchecked") {
-            task.style.display = "none";
-        }
+        (task.classList[1] === "checked") ?
+            task.style.display = "block"
+            : task.style.display = "none";
     }
-});
+}
 
-removeComplated.addEventListener("click", function () {
+let removeComplated = () => {
     let Tasks = document.querySelectorAll(".Task");
     for (let task of Tasks) {
         if (task.classList[1] === "checked") {
@@ -110,33 +98,37 @@ removeComplated.addEventListener("click", function () {
             let index = TaskList.indexOf(TaskName);
             TaskList.splice(index, 1);
             task.remove();
-            TaskCount--;
-            if (TaskCount == 0) {
+            if (TaskList.length == 0) {
                 listHeader.style.display = "none";
             }
         }
     }
-    saveData()
-});
+    showCount();
+    saveData();
+}
 
 function saveData() {
     localStorage.setItem("data", listContainer.innerHTML);
-    let Count = JSON.stringify(TaskCount)
-    localStorage.setItem("Count", Count);
-    let TaskArr = JSON.stringify(TaskList)
-    localStorage.setItem("TaskArr", TaskArr);
+    let TaskArr = JSON.stringify(TaskList);
+    localStorage.setItem("TaskList", TaskArr);
 }
 
 function showTask() {
     listContainer.innerHTML = localStorage.getItem("data");
-    let Count = localStorage.getItem("Count");
-    TaskCount = JSON.parse(Count)
-    let TaskArr = localStorage.getItem("TaskArr");
-    TaskList = JSON.parse(TaskArr);
-    if (TaskCount > 0) {
+    let TaskArr = localStorage.getItem("TaskList");
+    TaskList = JSON.parse(TaskArr) || [];
+    // (TaskList == null) ? TaskList = [] : "";
+    // console.log(TaskList);
+    if (TaskList.length > 0) {
         listHeader.style.display = "inline";
     }
+    showCount();
+    allTask();
 }
 
+function showCount() {
+    let totalTask = document.querySelector(".totalTask");
+    totalTask.innerHTML = TaskList.length;
+}
 
 showTask();
